317cf23db5d57235cf1e37ebd452aa7ac8f0eab1
ahsan.uddin <ahsan.uddin@gfk.com>
2023-09-08 08:58:17 +0500

    Error fixes

--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -1,12 +1,11 @@
 import Container from "@/components/container";
-import Experience from "@/components/experience";
+//import Experience from "@/components/experience";
 import Layout from "@/components/layout";
 
 export default function Page() {
   return (
     <Layout>
       <Container>
-        <Experience />
       </Container>
     </Layout>
   );
